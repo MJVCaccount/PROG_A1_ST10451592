2fb87f366aea625145870673cc936d363dda1675
MJani_Student_Account <ST10451592@vcconnect.edu.za>
2025-04-20 17:38:40 +0200

    Add project files.

---
+++ PROG_A1_ST10451592/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG_A1_ST10451592
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
